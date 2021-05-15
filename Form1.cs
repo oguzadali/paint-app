ba11abecd1c69120c770d231057c53dc8904fc63
oguzadali <ouzadali1@gmail.com>
2021-05-15 08:46:54 +0300

    form edit-functions

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,84 @@ namespace paint_app
 {
     public partial class Form1 : Form
     {
+        Bitmap bmp = new Bitmap(1024, 768);
+        Pen p = new Pen(Color.Black, 5);
+        bool drawing = false;
         public Form1()
         {
+       
             InitializeComponent();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (drawing)
+            {
+                drawing = false;
+            }
+            else
+            {
+                drawing = true;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drawing)
+            {
+                Graphics g = Graphics.FromImage(bmp);
+                g.DrawEllipse(p, e.X, e.Y, 3, 1);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Red;
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Blue;
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Yellow;
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Purple;
+        }
+
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Green;
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Jpeg Image|*.jpg|Bitmap Image *.bmp|";
+            saveFileDialog1.Title = "Save File";
+            saveFileDialog1.ShowDialog();
+
+            if (saveFileDialog1.FileName != "")
+            {
+                System.IO.FileStream sfile = (System.IO.FileStream)saveFileDialog1.OpenFile();
+                switch (saveFileDialog1.FilterIndex)
+                {
+                    case 1:
+                        this.pictureBox1.Image.Save(sfile, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                        break;
+                    case 2:
+                        this.pictureBox1.Image.Save(sfile, System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+
+                }
+            }
         }
     }
 }
